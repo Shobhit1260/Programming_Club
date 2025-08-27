b3a43eb1a1b5fe9c5d14269060bb02e548e01fb5
Shobhit Srivastava <151694752+Shobhit1260@users.noreply.github.com>
2025-08-27 17:20:51 +0530

    Update NewEvent.jsx

--- Ptsc_Frontend/src/components/Admin/Event/NewEvent.jsx
+++ Ptsc_Frontend/src/components/Admin/Event/NewEvent.jsx
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import React, { useState } from "react";
 import { toast } from "react-toastify";
-const BASE = "https://programming-club-backend.onrender.com";
+const BASE = "https://programming-club-zq7t.onrender.com";
 function NewEvent() {
   const [events, setEvents] = useState([]);
   const [formData, setFormData] = useState({
