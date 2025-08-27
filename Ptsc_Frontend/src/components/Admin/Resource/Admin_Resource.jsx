daac208ed6a25509b71583f5e82df5225ed05fb4
Shobhit Srivastava <151694752+Shobhit1260@users.noreply.github.com>
2025-08-27 17:22:36 +0530

    Update Admin_Resource.jsx

--- Ptsc_Frontend/src/components/Admin/Resource/Admin_Resource.jsx
+++ Ptsc_Frontend/src/components/Admin/Resource/Admin_Resource.jsx
@@ -3,7 +3,7 @@ import { useForm } from 'react-hook-form';
 import Resources from './Resources';
 import { toast } from 'react-toastify';
 
-const BASE = "https://programming-club-backend.onrender.com";
+const BASE = "https://programming-club-zq7t.onrender.com";
 
 const MediaUploadForm = () => {
   const [loading, setLoading] = useState(false);
