69c98d4ab5be437588d52807885d67baf681b6ba
Shobhit Srivastava <shobhitsri1405@gmail.com>
2025-08-28 22:13:02 +0530

    'auto'

--- Ptsc_Frontend/src/components/Utils/HomeCards.jsx
+++ Ptsc_Frontend/src/components/Utils/HomeCards.jsx
@@ -9,7 +9,7 @@ function Cards({title,description,buttonText,link}) {
     <div className='dark:bg-gray-900 font-serif flex flex-col items-start justify-center gap-4 w-1/3 h-max bg-white shadow-lg rounded-lg p-4 m-8'>
         <span className=' text-lg font-bold text-black dark:text-white'>{title}</span>
         <p className='text-gray-500 dark:text-white' >{desc}
-        {bool?<button onClick={()=>setBool(!bool)} className='text-blue-300'>more</button>:<button onClick={()=>setBool(!bool)} className='text-blue-300'>less</button>}
+        {/* {bool?<button onClick={()=>setBool(!bool)} className='text-blue-300'>more</button>:<button onClick={()=>setBool(!bool)} className='text-blue-300'>less</button>} */}
         </p>
         <Link to={`/${link}`} className='bg-white dark:bg-gray-900 border-2 shadow-lg p-2 text-md rounded-lg mt-4 dark:text-white'>{buttonText}</Link>
     </div>
