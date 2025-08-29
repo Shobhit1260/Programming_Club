234a2be1d33d621213e645e18a36e56c6a5878e5
Shobhit Srivastava <shobhitsri1405@gmail.com>
2025-08-29 07:33:23 +0530

    'auto'

--- Ptsc_Frontend/src/components/Pages/AllResources/Resources.jsx
+++ Ptsc_Frontend/src/components/Pages/AllResources/Resources.jsx
@@ -4,9 +4,9 @@ import { Outlet } from 'react-router-dom';
 function Resources() {
   return (
     <div className='w-full min-h-screen bg-gradient-to-b from-blue-200 to-white 
-      dark:from-gray-800 dark:to-gray-900 transition-colors duration-300 px-4 sm:px-8 py-12'>
+      dark:from-gray-800 dark:to-gray-900 transition-colors duration-300 px-4 sm:px-8 py-24'>
        
-       {/* Heading Section */}
+       
        <div className='w-full flex flex-col justify-center items-center gap-3 sm:gap-4 mb-10 sm:mb-16 text-center'>
             <h1 className='text-2xl sm:text-3xl lg:text-4xl text-black dark:text-white font-serif font-bold'>
               Resources
