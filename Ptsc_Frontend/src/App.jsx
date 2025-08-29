d37319072d6650253ec387ec5b6798eff495bdbb
Shobhit Srivastava <shobhitsri1405@gmail.com>
2025-08-29 08:06:32 +0530

    'auto'

--- Ptsc_Frontend/src/App.jsx
+++ Ptsc_Frontend/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
     <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-950 transition-colors duration-300">
       <Navbar />
 
-      <main className="flex-1 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+      <main className="flex-1 w-full mx-auto ">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
